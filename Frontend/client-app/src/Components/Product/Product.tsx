3d85663d9f28980de7b15b5ac8e2c733754d9e65
Cyril Kamgais <102966559+Kamgais@users.noreply.github.com>
2022-10-13 05:13:37 +0200

    (feature)shop-page-a1

--- Frontend/client-app/src/Components/Product/Product.tsx
+++ Frontend/client-app/src/Components/Product/Product.tsx
@@ -1,16 +1,10 @@
 import React, {FunctionComponent} from 'react';
+import { ProductDto } from '../../DTOs/ProductDto';
 import './product.styles/product.css'
 
 
 type Props = {
-    product : {
-    productUrl: string,
-    notes?:number,
-    title:string,
-    price:number;
-    reduction: number;
-
-    }
+    product : ProductDto
      // in percentage
 }
 
@@ -18,18 +12,18 @@ const Product:FunctionComponent<Props> = ({product}) => {
   return (
     <div className='product-container'>
         <div className="product-container-img">
-            <img src={product.productUrl} alt="product image" className="product-img" />
+            <img src={product.product_image} alt="product image" className="product-img" />
             <div className="product-container-actions">
                 <div className="in-cart"><i className="fa-solid fa-cart-shopping"></i></div>
                 <div className="like-product"><i className="fa-solid fa-thumbs-up"></i></div>
                 <div className="share-product"><i className="fa-solid fa-share"></i></div>
             </div>
-            <div className="product-reduction-label">{`-${product.reduction}%`}</div>
+            <div className="product-reduction-label">{`-${product.price}%`}</div>
         </div>
         <div className="product-container-infos">
             <div className="product-note"></div>
-            <div className="product-title">{product.title}</div>
-            <div className="product-price">{(product.price - (product.price * product.reduction) / 100).toFixed(2)}$  <span className='old-price'>{product.price}$</span></div>
+            <div className="product-title">{product.name}</div>
+            <div className="product-price">{(product.price - (product.price * product.price) / 100).toFixed(2)}$  <span className='old-price'>{product.price}$</span></div>
         </div>
       
     </div>
--- Frontend/client-app/src/Layouts/ProductList/ProductList.tsx
+++ Frontend/client-app/src/Layouts/ProductList/ProductList.tsx
@@ -1,11 +1,54 @@
-import React, {FunctionComponent} from 'react'
+import React, {FunctionComponent,useState,useEffect} from 'react'
+import Pagination from '../../Components/Pagination/Pagination';
+import Product from '../../Components/Product/Product';
+import { ProductDto } from '../../DTOs/ProductDto'
+import { ProductService } from '../../Services/ProductService';
+import Footer from '../Footer/Footer';
+import './products.styles/productList.css';
 
 const ProductList: FunctionComponent = () => {
+  const [products,setProducts] = useState<ProductDto[]|null>([]);
+
+  //user is currently on this page
+  const [currentPage,setCurrentPage] = useState(1);
+
+  //No of Records to be displayed on each page
+  const [recordsPerPage] = useState(10);
+
+  const indexOfLastRecord:number = currentPage * recordsPerPage;
+  const indexOfFirstRecord:number = indexOfLastRecord - recordsPerPage;
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await ProductService.getAllProducts();
+      setProducts(response!.data);
+      console.log(response!.data);
+    }
+
+    fetchProducts()
+  },[])
+
+
+  //Records to be displayed of the current page
+  if(products){
+    const currentRecords:ProductDto[] = products!.slice(indexOfFirstRecord,indexOfLastRecord);
+  }
+  
+  // number of pages
+  const nPages = Math.ceil(products!.length / recordsPerPage);
   return (
     <div className='product-list-container'>
+      <div className="product-list">
+        {
+          products!.map((product, index) => (
+            <Product product={product}/>
+          ))
+        }
+      </div>
+      <Pagination nPages={nPages}  currentPage={currentPage} setCurrentPage={setCurrentPage}/>
       
     </div>
   )
 }
 
-export default ProductList
+export default ProductList;
